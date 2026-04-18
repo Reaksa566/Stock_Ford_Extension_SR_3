1900f28fc6ce444d1d71ee4aaeef38be754553f1
Reaksa566 <ReaksaWelcome@gmail.com>
2026-04-18 23:03:53 +0700

    Merge remote changes

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\REAKSA-J\Desktop\stock_system_v2\venv
+set VIRTUAL_ENV=C:\Users\REAKSA-J\Desktop\stock_system_v3\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
